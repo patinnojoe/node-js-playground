cd91328926ea53a916dcf425ea77fec646094df4
patinnojoe <innocentjosiah57@gmail.com>
2025-08-23 05:38:32 +0100

    model a chema

--- index.js
+++ index.js
@@ -1,96 +1,42 @@
 const express = require('express');
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 const app = express();
-const connectionString = 'mongodb://localhost:27017';
-const client = new mongodb.MongoClient(connectionString);
-const db = client.db('studentDB');
-const student = db.collection('student');
-client
-  .connect()
+app.use(express.json());
+
+const connectionString = 'mongodb://localhost:27017/studentDB';
+mongoose
+  .connect(connectionString)
   .then(() => {
-    console.log('connection succeful');
+    console.log('connection successful');
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+    console.log(error);
   });
 
-app.use(express.json());
-app.set('view engine', 'ejs');
-app.use(express.json());
-
-const middleware1 = (req, res, nxt) => {
-  // throw new Error('error from 1');
-  console.log('MIDDLEWARE');
-  nxt();
-};
-const errorMiddleware = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('ERROOOORRR faced');
-};
-
-app.use(middleware1);
-
-app.get('/example', (req, res) => {
-  console.log(nameY);
-  res.send('You re on the home page but its cool');
-  // res.render('pages/index.ejs');
-});
-app.post('/student', (req, res) => {
-  const { name, age, email, dept } = req.body;
-  student
-    .insertMany(req.body)
-    .then(() => {
-      res.status(201).send('column added');
-    })
-    .catch((err) => {
-      res.status(500).send(err.message);
-    });
+const studentSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  dept: String,
 });
 
-app.get('/student', (req, res) => {
-  const { age } = req.query;
+const Student = mongoose.model('students', studentSchema);
 
-  student
-    .find({ age: parseInt(age) })
-    .toArray()
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      res.status(500).send(err.message);
-    });
+// single
+app.post('/student/signle', async (req, res, next) => {
+  try {
+    const { name, email, age, dept } = req.body;
+    const newStudent = new Student({ name, email, age, dept });
+    const data = await newStudent.save();
+
+    res.status(200).json({ message: 'student created', data });
+  } catch (error) {
+    res.status(500).json({ errorMesage: error.message });
+  }
 });
 
-app.put('/student', (req, res) => {
-  const { age } = req.query;
-  const { dept } = req.body;
-
-  student
-    .updateMany({ age: parseInt(age) }, { $set: { dept: dept } })
-    .then((data) => {
-      console.log(data);
-      res.status(200).json({ message: 'student updated uccefuly' });
-    })
-    .catch((err) => {
-      res.status(500).send(err.message);
-    });
-});
-
-app.delete('/student', (req, res) => {
-  const { email } = req.query;
-
-  student
-    .findOneAndDelete({ email })
-    .then((data) => {
-      console.log(data);
-      res.status(200).json({ message: 'student deleted succefuly' });
-    })
-    .catch((err) => {
-      res.status(500).send(err.message);
-    });
-});
-
-app.use(errorMiddleware);
+// multiple
+app.post('student/multiple', (req, res, next) => {});
 
 app.listen(8000, () => {
   console.log('server running onport 800');
